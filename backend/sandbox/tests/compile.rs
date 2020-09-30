6ecd94733ab18029f35564ef094db4147c10637b
virchau13 <virchau13@mail.hexular.net>
2020-09-30 13:18:35 +0800

    finished compiler sandbox

--- backend/sandbox/tests/compile.rs
+++ backend/sandbox/tests/compile.rs
@@ -2,8 +2,8 @@ extern crate sandbox;
 
 #[test]
 fn compile_test_success() {
-    let path: String = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/test.cpp";
-    sandbox::compile("/usr/bin/g++".to_string(), vec!["g++", "-std=c++17", "-O2", "-w", &path, "-o", "test"].iter().map(|x| x.to_string()).collect::<Vec<String>>()).unwrap();
+    let path: String = env!("CARGO_MANIFEST_DIR").to_string() + "/tests/test_compile.cpp";
+    sandbox::compile("/usr/bin/g++".to_string(), vec!["g++", "-std=c++17", "-O2", "-w", &path, "-o", "test.x86_64"].iter().map(|x| x.to_string()).collect::<Vec<String>>()).unwrap();
     // sandbox::compile("/usr/bin/env".to_string(), vec!["env"].iter().map(|x| x.to_string()).collect()).unwrap();
 }
 
